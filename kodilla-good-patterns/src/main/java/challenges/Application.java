9e4745e7a8e38f310dea22536c7fd4e3a8109f94
matimarkiewicz <mati.markiewicz@gmail.com>
2019-07-01 12:04:54 +0200

    modul_13_1

---
+++ kodilla-good-patterns/src/main/java/challenges/Application.java
@@ -0,0 +1,26 @@
+package challenges;
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.util.stream.Collectors;
+
+public class Application {
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        Map<String, List<String>> movieStoreMap = movieStore.getMovies();
+
+        final String booksMovieList = movieStoreMap.entrySet()
+                .stream()
+                .flatMap(n -> {
+                    List<String> list = new ArrayList<>();
+                    list.add(n.getKey());
+                    list.addAll(n.getValue());
+                    return list.stream();
+                })
+                .collect(Collectors.joining("! "));
+        System.out.println(booksMovieList);
+    }
+}
