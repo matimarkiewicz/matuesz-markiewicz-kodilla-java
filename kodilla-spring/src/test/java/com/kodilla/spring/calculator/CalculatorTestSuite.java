9cb46cdade12810edd2b31bfafcada5b8e1e3e4d
matimarkiewicz <mati.markiewicz@gmail.com>
2019-07-05 18:39:05 +0200

    modul_14_2

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.spring.calculator;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class CalculatorTestSuite {
+    @Test
+    public void testCalculator() {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+
+        double additionResult = calculator.addition(2,4);
+        double subtractionResult = calculator.subtraction(5,4);
+        double divisionResult = calculator.division(6,6);
+        double multiplication = calculator.multiplication(4,3);
+
+        Assert.assertEquals(6,additionResult,0.001);
+        Assert.assertEquals(1,subtractionResult,0.001);
+        Assert.assertEquals(1, divisionResult,0.001);
+        Assert.assertEquals(12, multiplication, 0.001);
+    }
+}
