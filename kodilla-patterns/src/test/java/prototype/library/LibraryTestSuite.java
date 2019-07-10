e3f52185b5e8aa7e635f247cbd536bb6c61cd65e
matimarkiewicz <mati.markiewicz@gmail.com>
2019-07-10 16:08:02 +0200

    modul_15_2 - poprawienie LocalDate

--- kodilla-patterns/src/test/java/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/prototype/library/LibraryTestSuite.java
@@ -5,14 +5,16 @@ import com.kodilla.patterns.prototype.library.Library;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 
 public class LibraryTestSuite {
     @Test
     public void testGetBooks() {
 
         Library library = new Library("Mati's Library");
-        Book firstBook = new Book("Duma i uprzedzenie", "Jane Austen", "1813");
-        Book secondBook = new Book("Wojna i pokój", "Lew Tołstoj", "1869");
+        Book firstBook = new Book("Duma i uprzedzenie", "Jane Austen", LocalDate.of(1813,1,1));
+        Book secondBook = new Book("Wojna i pokój", "Lew Tołstoj", LocalDate.of(1869,1,1));
 
         library.getBooks().add(firstBook);
         library.getBooks().add(secondBook);
@@ -35,7 +37,7 @@ public class LibraryTestSuite {
 
         library.getBooks().remove(secondBook);
 
-        Assert.assertEquals(2, library.getBooks().size());
+        Assert.assertEquals(1, library.getBooks().size());
         Assert.assertEquals(clonedLibrary.getBooks(), library.getBooks());
         Assert.assertNotEquals( deepClonedLibrary.getBooks(), library.getBooks());
     }
